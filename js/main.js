b66f6a044dabf8df8211b8376c606c1ccaade5fa
thienthai1 <surkessone@gmail.com>
2021-06-24 23:33:56 +0700

    psuh lastest

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    var sendList = [];
+    var noti = 0;
+
     $('#postLabel_submit').click(function() {
         
         var senderName = $('#postLabel_senderName').val();
@@ -58,7 +61,40 @@ $(document).ready(function(){
     });
 
     $('#postLabel_save').click(function() {
-        console.log("moo");
+        var myList = {
+            'senderName': $('#postLabel_senderName').val(),
+            'senderAddress' : $('#postLabel_senderAddress').val(),
+            'recieverName' : $('#postLabel_recieverName').val(),
+            'recieverAddress' : $('#postLabel_recieverAddress').val()
+        }
+        
+        sendList.push(myList);
+        var getTable = $('.listRecipient').children()[0]
+        var getBody = $(getTable).children()[0]
+        var getLength = sendList.length;
+        var tableTemplate = '<tr><td>' + $('#postLabel_senderName').val() + '</td><td>' + $('#postLabel_recieverName').val()  + '</td></tr>'
+        $(getBody).append(tableTemplate);
+        $('#postLabel_senderName').val('');
+        $('#postLabel_senderAddress').val('');
+        $('#postLabel_recieverName').val('');
+        $('#postLabel_recieverAddress').val('')
+        $('#postLabel_listLengthNumber').empty();
+        $('#postLabel_listLengthNumber').append(getLength);
+        $('#postLabel_notiList').toggle();
+        noti = 1;
+    });
+
+    $('#postLabel_openNameList').click(function() {
+        $('.listRecipient').css('display','block');
+        $('.listRecipient').focus();
+        if(noti == 1){
+            $('#postLabel_notiList').toggle();
+        }
+        noti = 0;
+    });
+
+    $('.listRecipient').blur(function() {
+        $('.listRecipient').css('display','none');
     });
 
 });
